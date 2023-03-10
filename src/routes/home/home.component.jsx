f9b8740ca2b951c66f7650de7ef98a52baf7cce7
Takara Truong <takaraktruong@gmail.com>
2023-03-10 11:58:34 -0800

    run test

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </div>
+  );
 }
