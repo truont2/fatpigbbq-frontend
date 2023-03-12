80440e1837b6f8d8828605449bdf76192957534e
Takara Truong <takaraktruong@gmail.com>
2023-03-11 22:12:32 -0800

    basic structure of the slider is complete

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,35 +1,86 @@
-import React from "react";
-import { Carousel } from "flowbite-react";
-import image from '../../assets/Fat-Pig-BBQ.jpeg'
+import React, { useState } from "react";
+// import { Carousel } from "flowbite-react";
+import image from "../../assets/DSC_7011.jpg";
+import image2 from "../../assets/DSC_7015.jpg";
+import image3 from "../../assets/DSC_7018.jpg";
+import image4 from "../../assets/DSC_7022.jpg";
+import image5 from "../../assets/DSC_7032.jpg";
+
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+// https://www.youtube.com/watch?v=tXlZCW26bto
 
 export default function Home() {
+  const slides = [
+    {
+      url: image,
+    },
+    {
+      url: image2,
+    },
+    {
+      url: image3,
+    },
+    {
+      url: image4,
+    },
+    {
+      url: image5,
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
-    <div style={{borderRadius: '0'}}>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-[50rem] rounded-none">
-        <Carousel>
-          <img
-            src={image}
-            alt=""
-          />
-          <img
-            src={image}
-            alt=""
-          />
-          <img
-            src={image}
-            alt=""
-          />
-          <img
-            src={image}
-            alt=""
-          />
-          <img
-            src={image}
-            alt=""
-          />
-        </Carousel>
+    <div>
+    <div className="max-w-screen h-[780px] w-screen m-auto relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full bg-center bg-cover duration-500"
+      ></div>
+      {/* left arrow */}
+      <div className="hidden group-hover:block text-white absolute top-[50%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20  cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
-      <a
+
+      {/* right arrow */}
+      <div className="hidden group-hover:block text-white absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20  cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+
+      {/* dots */}
+      <div className="absolute bottom-0 left-[50%] transform translate-x-1/2">
+      <div className="flex top-4 justify-center py-2 text-white">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+      </div>
+    </div>
+    <a
         className="App-link"
         href="https://reactjs.org"
         target="_blank"
