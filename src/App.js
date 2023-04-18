450ae7a2493b69d6f3fccda3c9221afd5a68e18c
Takara Truong <takaraktruong@gmail.com>
2023-04-18 14:31:34 -0700

    removed press error on app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Contact from "./routes/contact/contact";
 import Home from "./routes/home/home.component";
 import Menu from "./routes/menu/menu";
 import InProgress from "./routes/inprogress/InProgress";
-import Press from "./routes/press/Press";
 
 function App() {
   return (
